6e781224d6bc5e7dae62ca75000b4519ffaae6fd
Aika Mizuno Greer <aika.mizunogreer@gmail.com>
2025-11-20 10:20:11 +0000

    Scrapper Doctolib v2

--- scrapper_doctolib.py
+++ scrapper_doctolib.py
@@ -2,9 +2,12 @@
 # playwright install
 # sudo playwright install-deps
 
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, expect
+from playwright_stealth import stealth
 import pandas as pd
-import datetime
+import datetime, random, time
+
+## https://www.doctolib.fr/ivg-medicamenteuse/paris?availabilitiesBefore=90&regulationSector%5B%5D=CONTRACTED_1&regulationSector%5B%5D=CONTRACTED_1_WITH_EXTRA&regulationSector%5B%5D=CONTRACTED_1_WITH_OPTAM&regulationSector%5B%5D=CONTRACTED_1_WITH_OPTAM_CO&regulationSector%5B%5D=CONTRACTED_2&regulationSector%5B%5D=CONTRACTED_2_WITH_OPTAM&regulationSector%5B%5D=CONTRACTED_2_WITH_OPTAM_CO&regulationSector%5B%5D=CONTRACTED&regulationSector%5B%5D=CONTRACTED_WITH_EXTRA&regulationSector%5B%5D=ORGANIZATION_CONTRACTED
 
 columns = ["departement", "specialite", "trimestre", "mois", "deuxSemaines"]
 
@@ -18,24 +21,28 @@ listeDelais = ["90", "30", "14"]
 def getUrl(departement, specialite, delai):
     return baseUrl + "?location=" + departement + "&availabilitiesBefore=" + delai + "&speciality=" + specialite + argsSecteurs
 
-def extractData(browser, url):
-    context = browser.new_context()
+def extractData(context, url):
     page = context.new_page()
+    stealth(page)
     page.goto(url)
 
+    print(page.content())
+
     results = page.locator('[data-test-id="hcp-results"]')
-    results.locator('p').first.wait_for()
+    expect(results).to_have_text()
+    # results.locator('p').first.wait_for()
 
     textResultats = results.locator('p').first.inner_text()
     print(textResultats)
     effectif = int(textResultats.split(" ")[0])
-    context.close()
+    page.close()
 
     return effectif
 
 def main():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
+        context = browser.new_context()
         matrix = []
 
         for dep in listeDepartements:
@@ -43,21 +50,23 @@ def main():
                 row = [dep, spe]
 
                 for delai in listeDelais:
+                    time.sleep(random.uniform(1, 3))
                     url = getUrl(dep, spe, delai)
 
                     try:
-                        effectif = extractData(browser, url)
+                        effectif = extractData(context, url)
                     except:
                         effectif = 9999
                     row = row + [effectif]
 
                 matrix.append(row)
                 print(row)
-                
+            
+        context.close()
         browser.close()
         df = pd.DataFrame(matrix, columns=columns)
         
-        date = datetime.now().strftime("%Y-%m-%d-%H-%M")
+        date = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")
         
         df.to_csv(date + '.csv', index=False)
                     
