53fa76f85f383f15d63dbc9936057e4df09d97cf
Aika Mizuno Greer <aika.mizunogreer@gmail.com>
2025-12-05 08:24:56 +0000

    Organisation Dossier

--- scripts/scrapper_doctolib.py
+++ scripts/scrapper_doctolib.py
@@ -20,7 +20,6 @@ listeDepartements = ["Ile-de-France", "Bouche-du-Rhone", "Ain"]
 listeSpecialites = ["ivg-chirurgicale", "ivg-medicale-et-chirurgicale", "ivg-medicamenteuse"]
 listeDelais = ["90", "30", "14"]
 
-##### Variables : options for Playwright and semaphore for asyncio
 USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
